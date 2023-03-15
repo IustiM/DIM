c9b5e6909bf00db50becaef6d8680b0bb2572de5
Mutescu Iustinian <iustinianmutescu@gmail.com>
2023-03-15 13:22:54 +0200

    Am adugat resetarea parolei

--- DIM/settings.py
+++ DIM/settings.py
@@ -122,9 +122,16 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# SMTP configurations
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Tynu2210@gmail.com'
+EMAIL_HOST_PASSWORD = 'MotteLP2210'
